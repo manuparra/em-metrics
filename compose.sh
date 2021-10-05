758ed5596223a3ab8ad98913c54f49836123132b
Manu Parra <manuparra@Manus-MacBook-Air.local>
2021-10-05 11:38:24 +0200

    Compose file

---
+++ compose.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+## Append last data generated
+cat output/partial_metrics.csv >> output/power_metrics.csv
+rm output/partial_metrics.csv
+## Add to the repository
+git add output/power_metrics.csv
+git commit -m "'Update power consumption '$date"
+git push
+
