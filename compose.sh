13e6984851290de43da8e97530a61b2fdd6de222
Manu Parra <manuparra@Manus-MacBook-Air.local>
2021-10-05 11:45:38 +0200

    Commit data updated

--- compose.sh
+++ compose.sh
@@ -6,6 +6,6 @@ cat output/partial_metrics.csv >> output/power_metrics.csv
 rm output/partial_metrics.csv
 ## Add to the repository
 git add output/power_metrics.csv
-git commit -m "'Update power consumption '$date"
+git commit -m "Update power consumption `date`"
 git push
 
